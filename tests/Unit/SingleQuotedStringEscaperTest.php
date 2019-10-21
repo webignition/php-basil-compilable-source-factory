484053fd2d5021a3b401ad93853a515b3119cd03
Jon Cram <webignition@gmail.com>
2019-10-21 12:22:12 +0100

    Import SingleQuotedStringEscaper (#18)

---
+++ tests/Unit/SingleQuotedStringEscaperTest.php
@@ -0,0 +1,54 @@
+<?php
+/** @noinspection PhpDocSignatureInspection */
+/** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSourceFactory\Tests\Unit;
+
+use webignition\BasilCompilableSourceFactory\SingleQuotedStringEscaper;
+
+class SingleQuotedStringEscaperTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var SingleQuotedStringEscaper
+     */
+    private $escaper;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->escaper = SingleQuotedStringEscaper::create();
+    }
+
+    /**
+     * @dataProvider escapeDataProvider
+     */
+    public function testEscape(string $string, string $expectedEscapedString)
+    {
+        $this->assertEquals($expectedEscapedString, $this->escaper->escape($string));
+    }
+
+    public function escapeDataProvider(): array
+    {
+        return [
+            'no single quotes' => [
+                'string' => 'value',
+                'expectedEscapedString' => 'value',
+            ],
+            'encapsulated in single quotes' => [
+                'string' => "'value'",
+                'expectedEscapedString' => "\'value\'",
+            ],
+            'contains in single quotes' => [
+                'string' => "va'lu'e",
+                'expectedEscapedString' => "va\'lu\'e",
+            ],
+            'escaped single quotes' => [
+                'string' => "\'value\'",
+                'expectedEscapedString' => "\\\\\'value\\\\\'",
+            ],
+        ];
+    }
+}
