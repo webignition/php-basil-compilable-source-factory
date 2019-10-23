63f2ebba027f77981af3305b7c5d0cab9e69a285
Jon Cram <jon@webignition.net>
2019-10-23 09:30:47 +0100

    Add TranspilerInterface methods to HandlerInterface

--- src/HandlerInterface.php
+++ src/HandlerInterface.php
@@ -2,7 +2,23 @@
 
 namespace webignition\BasilCompilableSourceFactory;
 
+use webignition\BasilCompilableSourceFactory\Exception\NonTranspilableModelException;
+use webignition\BasilCompilationSource\SourceInterface;
+
 interface HandlerInterface
 {
+    // TranspileInterface methods
+    public static function createTranspiler();
+
+    /**
+     * @param object $model
+     *
+     * @return SourceInterface
+     *
+     * @throws NonTranspilableModelException
+     */
+    public function transpile(object $model): SourceInterface;
+
+    // HandlerInterface methods
     public function handles(object $model): bool;
 }
