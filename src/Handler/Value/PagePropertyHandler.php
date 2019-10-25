74e8a172dee5ac1a618cb737d63a4cbc5d35de68
Jon Cram <jon@webignition.net>
2019-10-25 16:00:05 +0100

    Remove direct 'transpile' uses

--- src/Handler/Value/PagePropertyHandler.php
+++ src/Handler/Value/PagePropertyHandler.php
@@ -19,7 +19,7 @@ class PagePropertyHandler implements HandlerInterface
     const PROPERTY_NAME_URL = 'url';
 
     private $variableDependencies;
-    private $transpiledValueMap;
+    private $contentMap;
 
     public function __construct()
     {
@@ -27,7 +27,7 @@ class PagePropertyHandler implements HandlerInterface
         $pantherClientPlaceholder = $this->variableDependencies->create(VariableNames::PANTHER_CLIENT);
         $pantherClientPlaceholderAsString = (string) $pantherClientPlaceholder;
 
-        $this->transpiledValueMap = [
+        $this->contentMap = [
             self::PROPERTY_NAME_TITLE => $pantherClientPlaceholderAsString . '->getTitle()',
             self::PROPERTY_NAME_URL => $pantherClientPlaceholderAsString . '->getCurrentURL()',
         ];
@@ -54,13 +54,13 @@ class PagePropertyHandler implements HandlerInterface
     public function createSource(object $model): SourceInterface
     {
         if ($this->handles($model) && $model instanceof ObjectValueInterface) {
-            $transpiledValue = $this->transpiledValueMap[$model->getProperty()] ?? null;
+            $statementContent = $this->contentMap[$model->getProperty()] ?? null;
 
-            if (is_string($transpiledValue)) {
+            if (is_string($statementContent)) {
                 $metadata = (new Metadata())
                     ->withVariableDependencies($this->variableDependencies);
 
-                return new Statement($transpiledValue, $metadata);
+                return new Statement($statementContent, $metadata);
             }
 
             throw new UnknownObjectPropertyException($model);
