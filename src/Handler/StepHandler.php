4d89e03ca851a49adf183f8110b2d7d41a3fc16b
Jon Cram <jon@webignition.net>
2019-10-29 13:35:40 +0000

    Handle step assertions

--- src/Handler/StepHandler.php
+++ src/Handler/StepHandler.php
@@ -4,26 +4,31 @@ namespace webignition\BasilCompilableSourceFactory\Handler;
 
 use webignition\BasilCompilableSourceFactory\Exception\UnsupportedModelException;
 use webignition\BasilCompilableSourceFactory\Handler\Action\ActionHandler;
+use webignition\BasilCompilableSourceFactory\Handler\Assertion\AssertionHandler;
 use webignition\BasilCompilableSourceFactory\HandlerInterface;
 use webignition\BasilCompilationSource\Comment;
 use webignition\BasilCompilationSource\EmptyLine;
 use webignition\BasilCompilationSource\LineList;
 use webignition\BasilCompilationSource\SourceInterface;
+use webignition\BasilModel\StatementInterface;
 use webignition\BasilModel\Step\StepInterface;
 
 class StepHandler implements HandlerInterface
 {
     private $actionHandler;
+    private $assertionHandler;
 
-    public function __construct(HandlerInterface $actionHandler)
+    public function __construct(HandlerInterface $actionHandler, HandlerInterface $assertionHandler)
     {
         $this->actionHandler = $actionHandler;
+        $this->assertionHandler = $assertionHandler;
     }
 
     public static function createHandler(): HandlerInterface
     {
         return new StepHandler(
-            ActionHandler::createHandler()
+            ActionHandler::createHandler(),
+            AssertionHandler::createHandler()
         );
     }
 
@@ -38,23 +43,23 @@ class StepHandler implements HandlerInterface
             throw new UnsupportedModelException($model);
         }
 
-        $statementList = new LineList([]);
+        $lineList = new LineList([]);
 
-        $actions = $model->getActions();
-        $actionCount = count($actions);
-        $hasMoreThanOneAction = $actionCount > 1;
-
-        foreach ($actions as $actionIndex => $action) {
-            $actionSource = $this->actionHandler->createSource($action);
-
-            $statementList->addLine(new Comment($action->getActionString()));
-            $statementList->addLines($actionSource->getLineObjects());
-
-            if ($hasMoreThanOneAction && $actionIndex < $actionCount -1) {
-                $statementList->addLine(new EmptyLine());
-            }
+        foreach ($model->getActions() as $action) {
+            $this->addSourceToLineList($lineList, $action, $this->actionHandler->createSource($action));
         }
 
-        return $statementList;
+        foreach ($model->getAssertions() as $assertion) {
+            $this->addSourceToLineList($lineList, $assertion, $this->assertionHandler->createSource($assertion));
+        }
+
+        return $lineList;
+    }
+
+    private function addSourceToLineList(LineList $lineList, StatementInterface $statement, SourceInterface $source)
+    {
+        $lineList->addLine(new Comment($statement->getSource()));
+        $lineList->addLines($source->getLineObjects());
+        $lineList->addLine(new EmptyLine());
     }
 }
