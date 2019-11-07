572ec5946b8a6ab9351e0e3544a2c8b88955be64
Jon Cram <jon@webignition.net>
2019-11-07 09:49:25 +0000

    Tidy up AbstractBrowserTestCase

--- tests/Functional/AbstractBrowserTestCase.php
+++ tests/Functional/AbstractBrowserTestCase.php
@@ -2,22 +2,11 @@
 
 namespace webignition\BasilCompilableSourceFactory\Tests\Functional;
 
-use Facebook\WebDriver\WebDriverDimension;
-use webignition\BasilCompilableSourceFactory\Tests\Services\CodeGenerator;
 use webignition\BasilCompilableSourceFactory\Tests\Services\TestCodeGenerator;
 use webignition\BasilCompilableSourceFactory\Tests\Services\TestRunner;
-use webignition\BasePantherTestCase\AbstractBrowserTestCase as BaseAbstractBrowserTestCase;
 
-abstract class AbstractBrowserTestCase extends BaseAbstractBrowserTestCase
+abstract class AbstractBrowserTestCase extends \PHPUnit\Framework\TestCase
 {
-    const FIXTURES_RELATIVE_PATH = '/Fixtures';
-    const FIXTURES_HTML_RELATIVE_PATH = '/html';
-
-    /**
-     * @var CodeGenerator
-     */
-    protected $codeGenerator;
-
     /**
      * @var TestRunner
      */
@@ -28,24 +17,9 @@ abstract class AbstractBrowserTestCase extends BaseAbstractBrowserTestCase
      */
     protected $testCodeGenerator;
 
-    public static function setUpBeforeClass(): void
-    {
-        self::$webServerDir = __DIR__
-            . '/..'
-            . self::FIXTURES_RELATIVE_PATH
-            . self::FIXTURES_HTML_RELATIVE_PATH;
-
-        parent::setUpBeforeClass();
-        self::$client->getWebDriver()->manage()->window()->setSize(new WebDriverDimension(1200, 1100));
-    }
-
     protected function setUp(): void
     {
-        $this->codeGenerator = CodeGenerator::create();
         $this->testRunner = new TestRunner();
         $this->testCodeGenerator = TestCodeGenerator::create();
-
-        self::stopWebServer();
-        self::$client->quit();
     }
 }
