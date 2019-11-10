bd3740a051e55663eeb47059b79d522634126696
Jon Cram <jon@webignition.net>
2019-11-10 12:43:50 +0000

    Remove BrowserPropertyHandler

--- src/Handler/Value/ScalarValueHandler.php
+++ src/Handler/Value/ScalarValueHandler.php
@@ -4,26 +4,26 @@ namespace webignition\BasilCompilableSourceFactory\Handler\Value;
 
 use webignition\BasilCompilableSourceFactory\Exception\UnknownObjectPropertyException;
 use webignition\BasilCompilableSourceFactory\Exception\UnsupportedModelException;
+use webignition\BasilCompilableSourceFactory\VariableNames;
 use webignition\BasilCompilationSource\Block\Block;
 use webignition\BasilCompilationSource\Block\BlockInterface;
 use webignition\BasilCompilationSource\Line\Statement;
+use webignition\BasilCompilationSource\Metadata\Metadata;
+use webignition\BasilCompilationSource\VariablePlaceholderCollection;
 use webignition\BasilModel\Value\ObjectValueInterface;
 use webignition\BasilModel\Value\ObjectValueType;
 
 class ScalarValueHandler
 {
-    private $browserPropertyHandler;
     private $environmentValueHandler;
     private $literalValueHandler;
     private $pagePropertyHandler;
 
     public function __construct(
-        BrowserPropertyHandler $browserPropertyHandler,
         EnvironmentValueHandler $environmentValueHandler,
         LiteralValueHandler $literalValueHandler,
         PagePropertyHandler $pagePropertyHandler
     ) {
-        $this->browserPropertyHandler = $browserPropertyHandler;
         $this->environmentValueHandler = $environmentValueHandler;
         $this->literalValueHandler = $literalValueHandler;
         $this->pagePropertyHandler = $pagePropertyHandler;
@@ -32,7 +32,6 @@ class ScalarValueHandler
     public static function createHandler(): ScalarValueHandler
     {
         return new ScalarValueHandler(
-            new BrowserPropertyHandler(),
             new EnvironmentValueHandler(),
             new LiteralValueHandler(),
             new PagePropertyHandler()
@@ -49,8 +48,12 @@ class ScalarValueHandler
      */
     public function handle(object $model): BlockInterface
     {
-        if ($model instanceof ObjectValueInterface && ObjectValueType::BROWSER_PROPERTY === $model->getType()) {
-            return $this->browserPropertyHandler->handle($model);
+        $isBrowserProperty = $model instanceof ObjectValueInterface
+            && ObjectValueType::BROWSER_PROPERTY === $model->getType()
+            && 'size' === $model->getProperty();
+
+        if ($isBrowserProperty) {
+            return $this->handleFoo();
         }
 
         if ($model instanceof ObjectValueInterface && $model->getType() === ObjectValueType::DATA_PARAMETER) {
@@ -72,5 +75,32 @@ class ScalarValueHandler
         }
 
         throw new UnsupportedModelException($model);
+    }
+
+    private function handleFoo()
+    {
+        $variableExports = new VariablePlaceholderCollection();
+        $webDriverDimensionPlaceholder = $variableExports->create('WEBDRIVER_DIMENSION');
+
+        $variableDependencies = new VariablePlaceholderCollection();
+        $pantherClientPlaceholder = $variableDependencies->create(VariableNames::PANTHER_CLIENT);
+
+        $dimensionAssignment = new Statement(
+            sprintf(
+                '%s = %s->getWebDriver()->manage()->window()->getSize()',
+                $webDriverDimensionPlaceholder,
+                $pantherClientPlaceholder
+            ),
+            (new Metadata())
+                ->withVariableDependencies($variableDependencies)
+                ->withVariableExports($variableExports)
+        );
+
+        $getWidthCall = $webDriverDimensionPlaceholder . '->getWidth()';
+        $getHeightCall = $webDriverDimensionPlaceholder . '->getHeight()';
+
+        $dimensionConcatenation = new Statement('(string) ' . $getWidthCall . ' . \'x\' . (string) ' . $getHeightCall);
+
+        return new Block([$dimensionAssignment, $dimensionConcatenation]);
     }
 }
