b8eb666c60cef965fa5597fd4fb7bf63e89c8ab2
Jon Cram <jon@webignition.net>
2020-02-22 19:21:38 +0000

    Create AssertionMethodInvocationFactory

---
+++ src/AssertionMethodInvocationFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSourceFactory;
+
+use webignition\BasilCompilableSource\Line\LiteralExpression;
+use webignition\BasilCompilableSource\Line\MethodInvocation\ObjectMethodInvocation;
+use webignition\BasilCompilableSource\VariablePlaceholder;
+
+class AssertionMethodInvocationFactory
+{
+    private $singleQuotedStringEscaper;
+
+    public function __construct(SingleQuotedStringEscaper $singleQuotedStringEscaper)
+    {
+        $this->singleQuotedStringEscaper = $singleQuotedStringEscaper;
+    }
+
+    public static function createFactory(): AssertionMethodInvocationFactory
+    {
+        return new AssertionMethodInvocationFactory(
+            SingleQuotedStringEscaper::create()
+        );
+    }
+
+    public function create(
+        string $assertionMethod,
+        array $arguments,
+        string $failureMessage = ''
+    ): ObjectMethodInvocation {
+        if ('' !== $failureMessage) {
+            $arguments[] = new LiteralExpression(
+                '\'' . $this->singleQuotedStringEscaper->escape($failureMessage) . '\''
+            );
+        }
+
+        return new ObjectMethodInvocation(
+            VariablePlaceholder::createDependency(VariableNames::PHPUNIT_TEST_CASE),
+            $assertionMethod,
+            $arguments
+        );
+    }
+}
