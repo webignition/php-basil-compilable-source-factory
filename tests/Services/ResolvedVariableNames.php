f90e42535ddf118960b416a9fec36c0fa595e3d5
Jon Cram <jon@webignition.net>
2020-02-25 14:48:15 +0000

    Use base test examinedValue and expectedValue

--- tests/Services/ResolvedVariableNames.php
+++ tests/Services/ResolvedVariableNames.php
@@ -6,8 +6,8 @@ namespace webignition\BasilCompilableSourceFactory\Tests\Services;
 
 class ResolvedVariableNames
 {
-    public const EXAMINED_VALUE_VARIABLE_NAME = '$examinedValue';
-    public const EXPECTED_VALUE_VARIABLE_NAME = '$expectedValue';
+    public const EXAMINED_VALUE_VARIABLE_NAME = '$this->examinedValue';
+    public const EXPECTED_VALUE_VARIABLE_NAME = '$this->expectedValue';
     public const HAS_VARIABLE_NAME = '$has';
     public const ENV_ARRAY_VARIABLE_NAME = '$_ENV';
     public const WEBDRIVER_DIMENSION_VARIABLE_NAME = '$webDriverDimension';
