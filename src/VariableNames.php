3f3dcd1adab24e877f4f22066d3762edd104054b
Jon Cram <webignition@gmail.com>
2020-04-10 15:03:10 +0100

    Create StatementFactoryCallFactory

--- src/VariableNames.php
+++ src/VariableNames.php
@@ -13,4 +13,6 @@ class VariableNames
     public const WEBDRIVER_ELEMENT_INSPECTOR = 'INSPECTOR';
     public const WEBDRIVER_ELEMENT_MUTATOR = 'MUTATOR';
     public const PANTHER_CRAWLER = 'CRAWLER';
+    public const ACTION_FACTORY = 'ACTION_FACTORY';
+    public const ASSERTION_FACTORY = 'ASSERTION_FACTORY';
 }
---
+++ tests/Unit/CallFactory/StatementFactoryCallFactoryTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSourceFactory\Tests\Unit\CallFactory;
+
+use webignition\BasilCompilableSource\Metadata\Metadata;
+use webignition\BasilCompilableSource\Metadata\MetadataInterface;
+use webignition\BasilCompilableSource\VariablePlaceholderCollection;
+use webignition\BasilCompilableSourceFactory\CallFactory\StatementFactoryCallFactory;
+use webignition\BasilCompilableSourceFactory\VariableNames;
+use webignition\BasilModels\Assertion\DerivedElementExistsAssertion;
+use webignition\BasilModels\StatementInterface;
+use webignition\BasilParser\ActionParser;
+use webignition\BasilParser\AssertionParser;
+
+class StatementFactoryCallFactoryTest extends \PHPUnit\Framework\TestCase
+{
+    private $factory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->factory = StatementFactoryCallFactory::createFactory();
+    }
+
+    /**
+     * @dataProvider createStatementFactoryCallDataProvider
+     */
+    public function testCreateStatementFactoryCall(
+        StatementInterface $statement,
+        string $expectedRenderedContent,
+        MetadataInterface $expectedMetadata
+    ) {
+        $objectMethodInvocation = $this->factory->create($statement);
+
+        $this->assertEquals($expectedRenderedContent, $objectMethodInvocation->render());
+        $this->assertEquals($expectedMetadata, $objectMethodInvocation->getMetadata());
+    }
+
+    public function createStatementFactoryCallDataProvider(): array
+    {
+        $actionParser = ActionParser::create();
+        $assertionParser = AssertionParser::create();
+
+        return [
+            'click action' => [
+                'statement' => $actionParser->parse('click $".selector"'),
+                'expectedRenderedSource' =>
+                    '{{ ACTION_FACTORY }}->createFromJson(\'{' .  "\n" .
+                    '    "source": "click $\\\\".selector\\\\"",' . "\n" .
+                    '    "type": "click",' . "\n" .
+                    '    "arguments": "$\\\\".selector\\\\"",' . "\n" .
+                    '    "identifier": "$\\\\".selector\\\\""' . "\n" .
+                    '}\')'
+                ,
+                'expectedMetadata' => new Metadata([
+                    Metadata::KEY_VARIABLE_DEPENDENCIES => VariablePlaceholderCollection::createDependencyCollection([
+                        VariableNames::ACTION_FACTORY,
+                    ]),
+                ]),
+            ],
+            'exists assertion' => [
+                'statement' => $assertionParser->parse('$".selector" exists'),
+                'expectedRenderedSource' =>
+                    '{{ ASSERTION_FACTORY }}->createFromJson(\'{' . "\n" .
+                    '    "source": "$\\\\".selector\\\\" exists",' . "\n" .
+                    '    "identifier": "$\\\\".selector\\\\"",' . "\n" .
+                    '    "comparison": "exists"' . "\n" .
+                    '}\')'
+                ,
+                'expectedMetadata' => new Metadata([
+                    Metadata::KEY_VARIABLE_DEPENDENCIES => VariablePlaceholderCollection::createDependencyCollection([
+                        VariableNames::ASSERTION_FACTORY,
+                    ]),
+                ]),
+            ],
+            'derived exists assertion' => [
+                'statement' => new DerivedElementExistsAssertion(
+                    $actionParser->parse('click $".selector"'),
+                    '$".selector"'
+                ),
+                'expectedRenderedSource' =>
+                    '{{ ASSERTION_FACTORY }}->createFromJson(\'{' . "\n" .
+                    '    "source_type": "action",' . "\n" .
+                    '    "source": {' . "\n" .
+                    '        "source": "click $\\\\".selector\\\\"",' . "\n" .
+                    '        "type": "click",' . "\n" .
+                    '        "arguments": "$\\\\".selector\\\\"",' . "\n" .
+                    '        "identifier": "$\\\\".selector\\\\""' . "\n" .
+                    '    },' . "\n" .
+                    '    "identifier": "$\\\\".selector\\\\""' . "\n" .
+                    '}\')'
+                ,
+                'expectedMetadata' => new Metadata([
+                    Metadata::KEY_VARIABLE_DEPENDENCIES => VariablePlaceholderCollection::createDependencyCollection([
+                        VariableNames::ASSERTION_FACTORY,
+                    ]),
+                ]),
+            ],
+        ];
+    }
+}
