00faee8cc730c40bd099854023e926713bfd2dce
Jon Cram <webignition@gmail.com>
2020-06-04 16:19:18 +0100

    Add indenting to ElementIdentifierSerializer (#489)

--- src/ElementIdentifierSerializer.php
+++ src/ElementIdentifierSerializer.php
@@ -14,10 +14,22 @@ class ElementIdentifierSerializer
         return new ElementIdentifierSerializer();
     }
 
-    public function serialize(ElementIdentifierInterface $elementIdentifier): string
+    public function serialize(ElementIdentifierInterface $elementIdentifier, int $indentDepth = 0): string
     {
         $elementOnlyIdentifier = ElementIdentifier::fromAttributeIdentifier($elementIdentifier);
+        $serializedSourceIdentifier = (string) json_encode($elementOnlyIdentifier, JSON_PRETTY_PRINT);
 
-        return (string) json_encode($elementOnlyIdentifier, JSON_PRETTY_PRINT);
+        if ($indentDepth > 0) {
+            $indent = str_repeat('    ', $indentDepth);
+
+            $serializedSourceIdentifierLines = explode("\n", $serializedSourceIdentifier);
+            array_walk($serializedSourceIdentifierLines, function (&$line) use ($indent) {
+                $line = $indent . $line;
+            });
+
+            $serializedSourceIdentifier = implode("\n", $serializedSourceIdentifierLines);
+        }
+
+        return $serializedSourceIdentifier;
     }
 }
---
+++ tests/Unit/ElementIdentifierSerializerTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSourceFactory\Tests\Unit;
+
+use webignition\BasilCompilableSourceFactory\ElementIdentifierSerializer;
+use webignition\DomElementIdentifier\ElementIdentifier;
+use webignition\DomElementIdentifier\ElementIdentifierInterface;
+
+class ElementIdentifierSerializerTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider serializeDataProvider
+     */
+    public function testSerialize(
+        ElementIdentifierInterface $elementIdentifier,
+        int $indentDepth,
+        string $expectedSerializedIdentifier
+    ) {
+        $serializer = ElementIdentifierSerializer::createSerializer();
+
+        $this->assertSame($expectedSerializedIdentifier, $serializer->serialize($elementIdentifier, $indentDepth));
+    }
+
+    public function serializeDataProvider(): array
+    {
+        return [
+            'selector only, no indent' => [
+                'elementIdentifier' => new ElementIdentifier('.selector'),
+                'indentDepth' => 0,
+                'expectedSerializedIdentifier' =>
+                    '{' . "\n" .
+                    '    "locator": ".selector"' . "\n" .
+                    '}'
+                ,
+            ],
+            'selector, position, no indent' => [
+                'elementIdentifier' => new ElementIdentifier('.selector', 2),
+                'indentDepth' => 0,
+                'expectedSerializedIdentifier' =>
+                    '{' . "\n" .
+                    '    "locator": ".selector",' . "\n" .
+                    '    "position": 2' . "\n" .
+                    '}'
+                ,
+            ],
+            'selector, position, parent, no indent' => [
+                'elementIdentifier' => (
+                    new ElementIdentifier('.child', 2))
+                    ->withParentIdentifier(
+                        new ElementIdentifier('.parent', 3)
+                    ),
+                'indentDepth' => 0,
+                'expectedSerializedIdentifier' =>
+                    '{' . "\n" .
+                    '    "locator": ".child",' . "\n" .
+                    '    "parent": {' . "\n" .
+                    '        "locator": ".parent",' . "\n" .
+                    '        "position": 3' . "\n" .
+                    '    },' . "\n" .
+                    '    "position": 2' . "\n" .
+                    '}'
+                ,
+            ],
+            'selector only, indent=1' => [
+                'elementIdentifier' => new ElementIdentifier('.selector'),
+                'indentDepth' => 1,
+                'expectedSerializedIdentifier' =>
+                    '    {' . "\n" .
+                    '        "locator": ".selector"' . "\n" .
+                    '    }'
+                ,
+            ],
+            'selector, position, indent=1' => [
+                'elementIdentifier' => new ElementIdentifier('.selector', 2),
+                'indentDepth' => 1,
+                'expectedSerializedIdentifier' =>
+                    '    {' . "\n" .
+                    '        "locator": ".selector",' . "\n" .
+                    '        "position": 2' . "\n" .
+                    '    }'
+                ,
+            ],
+            'selector, position, parent, indent=1' => [
+                'elementIdentifier' => (
+                new ElementIdentifier('.child', 2))
+                    ->withParentIdentifier(
+                        new ElementIdentifier('.parent', 3)
+                    ),
+                'indentDepth' => 1,
+                'expectedSerializedIdentifier' =>
+                    '    {' . "\n" .
+                    '        "locator": ".child",' . "\n" .
+                    '        "parent": {' . "\n" .
+                    '            "locator": ".parent",' . "\n" .
+                    '            "position": 3' . "\n" .
+                    '        },' . "\n" .
+                    '        "position": 2' . "\n" .
+                    '    }'
+                ,
+            ],
+            'selector only, indent=2' => [
+                'elementIdentifier' => new ElementIdentifier('.selector'),
+                'indentDepth' => 2,
+                'expectedSerializedIdentifier' =>
+                    '        {' . "\n" .
+                    '            "locator": ".selector"' . "\n" .
+                    '        }'
+                ,
+            ],
+        ];
+    }
+}
