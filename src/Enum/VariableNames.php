314fb4ad307b671a068c0d1b5521a498ffb5501d
Jon Cram <webignition@gmail.com>
2025-12-12 14:45:48 +0000

    Create enum

---
+++ src/Enum/VariableNames.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSourceFactory\Enum;
+
+enum VariableNames: string
+{
+    case PANTHER_CLIENT = 'CLIENT';
+    case ENVIRONMENT_VARIABLE_ARRAY = 'ENV';
+    case DOM_CRAWLER_NAVIGATOR = 'NAVIGATOR';
+    case PHPUNIT_TEST_CASE = 'PHPUNIT';
+    case WEBDRIVER_ELEMENT_INSPECTOR = 'INSPECTOR';
+    case WEBDRIVER_ELEMENT_MUTATOR = 'MUTATOR';
+    case PANTHER_CRAWLER = 'CRAWLER';
+    case ACTION_FACTORY = 'ACTION_FACTORY';
+    case ASSERTION_FACTORY = 'ASSERTION_FACTORY';
+}
