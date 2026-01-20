34056caa1b01fe772f66de73fe5b6323e3a10715
Jon Cram <webignition@gmail.com>
2026-01-20 16:08:06 +0000

    Model constant/enum access

---
+++ src/Model/Expression/ObjectConstant.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSourceFactory\Model\Expression;
+
+use webignition\BasilCompilableSourceFactory\Model\ClassName;
+use webignition\BasilCompilableSourceFactory\Model\Metadata\Metadata;
+use webignition\BasilCompilableSourceFactory\Model\Metadata\MetadataInterface;
+
+readonly class ObjectConstant implements ExpressionInterface
+{
+    private const RENDER_TEMPLATE = '{{ class }}::{{ property }}';
+
+    public function __construct(
+        private ClassName $className,
+        private string $property,
+    ) {}
+
+    public function getTemplate(): string
+    {
+        return self::RENDER_TEMPLATE;
+    }
+
+    public function getContext(): array
+    {
+        return [
+            'class' => $this->className->renderClassName(),
+            'property' => $this->property,
+        ];
+    }
+
+    public function getMetadata(): MetadataInterface
+    {
+        return new Metadata([
+            $this->className->getClassName(),
+        ]);
+    }
+}
