7ed24245b8c656f5fbd49b222a4db71e1f8528bf
Jon Cram <webignition@gmail.com>
2026-01-21 14:57:20 +0000

    Add scalar not-exists tests (#866)

---
+++ tests/DataProvider/Assertion/CreateFromScalarNotExistsAssertionDataProviderTrait.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSourceFactory\Tests\DataProvider\Assertion;
+
+use webignition\BasilCompilableSourceFactory\Enum\VariableName;
+use webignition\BasilCompilableSourceFactory\Model\Metadata\Metadata;
+use webignition\BasilModels\Parser\AssertionParser;
+
+trait CreateFromScalarNotExistsAssertionDataProviderTrait
+{
+    /**
+     * @return array<mixed>
+     */
+    public static function createFromScalarNotExistsAssertionDataProvider(): array
+    {
+        $assertionParser = AssertionParser::create();
+
+        return [
+            'not-exists comparison, page property examined value' => [
+                'statement' => $assertionParser->parse('$page.url not-exists', 0),
+                'expectedRenderedSetup' => <<< 'EOD'
+                    $examinedValue = ({{ CLIENT }}->getCurrentURL() ?? null) !== null;
+                    EOD,
+                'expectedRenderedBody' => <<< 'EOD'
+                    {{ PHPUNIT }}->assertFalse(
+                        $examinedValue,
+                        '{
+                            "statement": {
+                                "statement-type": "assertion",
+                                "source": "$page.url not-exists",
+                                "index": 0,
+                                "identifier": "$page.url",
+                                "operator": "not-exists"
+                            },
+                            "expected": ' . (false ? 'true' : 'false') . ',
+                            "examined": ' . ($examinedValue ? 'true' : 'false') . '
+                        }',
+                    );
+                    EOD,
+                'expectedSetupMetadata' => new Metadata(
+                    variableNames: [
+                        VariableName::PANTHER_CLIENT,
+                    ],
+                ),
+                'expectedBodyMetadata' => new Metadata(
+                    variableNames: [
+                        VariableName::PHPUNIT_TEST_CASE,
+                    ],
+                ),
+            ],
+            'not-exists comparison, data parameter value' => [
+                'statement' => $assertionParser->parse('$data.key not-exists', 0),
+                'expectedRenderedSetup' => <<< 'EOD'
+                    $examinedValue = ($key ?? null) !== null;
+                    EOD,
+                'expectedRenderedBody' => <<< 'EOD'
+                    {{ PHPUNIT }}->assertFalse(
+                        $examinedValue,
+                        '{
+                            "statement": {
+                                "statement-type": "assertion",
+                                "source": "$data.key not-exists",
+                                "index": 0,
+                                "identifier": "$data.key",
+                                "operator": "not-exists"
+                            },
+                            "expected": ' . (false ? 'true' : 'false') . ',
+                            "examined": ' . ($examinedValue ? 'true' : 'false') . '
+                        }',
+                    );
+                    EOD,
+                'expectedSetupMetadata' => new Metadata(),
+                'expectedBodyMetadata' => new Metadata(
+                    variableNames: [
+                        VariableName::PHPUNIT_TEST_CASE,
+                    ],
+                ),
+            ],
+        ];
+    }
+}
