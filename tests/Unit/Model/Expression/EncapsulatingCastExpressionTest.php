0137621267ce04d9ca1ed0e42289e8296e11d398
Jon Cram <webignition@gmail.com>
2026-01-23 18:37:15 +0000

    Remove ObjectPropertyAccessExpression (#895)

--- tests/Unit/Model/Expression/EncapsulatingCastExpressionTest.php
+++ tests/Unit/Model/Expression/EncapsulatingCastExpressionTest.php
@@ -13,7 +13,6 @@ use webignition\BasilCompilableSourceFactory\Model\Expression\ComparisonExpressi
 use webignition\BasilCompilableSourceFactory\Model\Expression\CompositeExpression;
 use webignition\BasilCompilableSourceFactory\Model\Expression\EncapsulatingCastExpression;
 use webignition\BasilCompilableSourceFactory\Model\Expression\LiteralExpression;
-use webignition\BasilCompilableSourceFactory\Model\Expression\ObjectPropertyAccessExpression;
 use webignition\BasilCompilableSourceFactory\Model\MethodInvocation\MethodInvocation;
 use webignition\BasilCompilableSourceFactory\Model\MethodInvocation\ObjectMethodInvocation;
 use webignition\BasilCompilableSourceFactory\Model\StaticObject;
@@ -86,16 +85,6 @@ class EncapsulatingCastExpressionTest extends AbstractResolvableTestCase
                     'string'
                 ),
                 'expectedString' => '(string) ($_ENV["secret"])',
-            ],
-            'object property access expression as string' => [
-                'expression' => new EncapsulatingCastExpression(
-                    new ObjectPropertyAccessExpression(
-                        new VariableDependency(VariableName::PANTHER_CLIENT),
-                        'property'
-                    ),
-                    'string'
-                ),
-                'expectedString' => '(string) ({{ CLIENT }}->property)',
             ],
             'method invocation as string' => [
                 'expression' => new EncapsulatingCastExpression(
