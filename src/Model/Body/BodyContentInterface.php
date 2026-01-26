1d922f842bcd6baa09ac1945a28d49f4b215a079
Jon Cram <webignition@gmail.com>
2026-01-26 10:29:13 +0000

    Add mightThrow() to body interfaces

--- src/Model/Body/BodyContentInterface.php
+++ src/Model/Body/BodyContentInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace webignition\BasilCompilableSourceFactory\Model\Body;
 
+use webignition\BasilCompilableSourceFactory\Model\MightThrowInterface;
 use webignition\Stubble\Resolvable\ResolvableInterface;
 
-interface BodyContentInterface extends ResolvableInterface {}
+interface BodyContentInterface extends ResolvableInterface, MightThrowInterface {}
--- src/Model/Body/BodyInterface.php
+++ src/Model/Body/BodyInterface.php
@@ -5,5 +5,6 @@ declare(strict_types=1);
 namespace webignition\BasilCompilableSourceFactory\Model\Body;
 
 use webignition\BasilCompilableSourceFactory\Model\HasMetadataInterface;
+use webignition\BasilCompilableSourceFactory\Model\MightThrowInterface;
 
 interface BodyInterface extends BodyContentInterface, HasMetadataInterface {}
