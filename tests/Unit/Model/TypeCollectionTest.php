00c6be4944f45824528bbfc579795ae744e5fbe3
Jon Cram <webignition@gmail.com>
2026-01-30 14:38:21 +0000

    Add TypeCollection::equals() (#946)

--- tests/Unit/Model/TypeCollectionTest.php
+++ tests/Unit/Model/TypeCollectionTest.php
@@ -37,4 +37,39 @@ class TypeCollectionTest extends AbstractResolvableTestCase
             ],
         ];
     }
+
+    #[DataProvider('equalsDataProvider')]
+    public function testEquals(TypeCollection $collection1, TypeCollection $collection2, bool $expected): void
+    {
+        self::assertSame($expected, $collection1->equals($collection2));
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public static function equalsDataProvider(): array
+    {
+        return [
+            'both contain same single type' => [
+                'collection1' => new TypeCollection([Type::STRING]),
+                'collection2' => new TypeCollection([Type::STRING]),
+                'expected' => true,
+            ],
+            'both contain different single type (1)' => [
+                'collection1' => new TypeCollection([Type::INTEGER]),
+                'collection2' => new TypeCollection([Type::STRING]),
+                'expected' => false,
+            ],
+            'both contain different single type (2)' => [
+                'collection1' => new TypeCollection([Type::STRING]),
+                'collection2' => new TypeCollection([Type::INTEGER]),
+                'expected' => false,
+            ],
+            'both contain different multiple types' => [
+                'collection1' => new TypeCollection([Type::STRING, Type::INTEGER, Type::BOOLEAN]),
+                'collection2' => new TypeCollection([Type::INTEGER, Type::BOOLEAN, Type::NULL]),
+                'expected' => false,
+            ],
+        ];
+    }
 }
